e908a262c73dbed949b6d238721f7ddd7e1cf9e2
Georgi Mirchev <gmirchev@melontech.com>
2021-05-21 17:41:45 +0300

    _Layout tabs fix names and functionality

--- HomeTreatment/ViewModels/RegisterViewModel.cs
+++ HomeTreatment/ViewModels/RegisterViewModel.cs
@@ -4,20 +4,20 @@ namespace HomeTreatment.ViewModels
 {
     public class RegisterViewModel
     {
-        [Required(ErrorMessage = "Please enter your first name")]
+        [Required(ErrorMessage = "Please fill your First Name")]
         public string FirstName { get; set; }
 
-        [Required(ErrorMessage = "Please enter your last name")]
+        [Required(ErrorMessage = "Please fill your Last Name")]
         public string LastName { get; set; }
 
-        [Required(ErrorMessage = "Please enter your email address")]
+        [Required(ErrorMessage = "Please fill your Email Address")]
         [EmailAddress]
         public string Email { get; set; }
 
-        [Required(ErrorMessage = "Please enter your password")]
+        [Required(ErrorMessage = "Please fill your Password")]
         public string Password { get; set; }
 
-        [Required(ErrorMessage = "Please confirm your password")]
+        [Required(ErrorMessage = "Please confirm your Password")]
         [Compare(nameof(Password))]
         public string ConfirmPassword { get; set; }
 
