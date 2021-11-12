1f41ed832a6a0640d2f9c822e61becfc5eef361a
Georgi Mirchev <94171134+GeorgiMrchv@users.noreply.github.com>
2021-11-12 17:55:01 +0200

    Update UserRole.cs

--- HomeTreatment/Infrastructure/Enum/UserRole.cs
+++ HomeTreatment/Infrastructure/Enum/UserRole.cs
@@ -1,17 +1,12 @@
 ﻿namespace HomeTreatment.Web.Infrastructure
-{
-   
+{   
     public enum UserRole
     {       
         Admin = 1,
         User = 2,
         Patient = 3,
         Doctor = 4       
-
-
     }
-
-    
 }
 
 // nikoga da ne slagam 0 za default value, poneve ako deserilize nqma ekvivalentna stoinost v JSON-a
